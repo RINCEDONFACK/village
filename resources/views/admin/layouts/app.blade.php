f7b0b2a0e6e338558d06a41b898607f9eb1bc420
rincedonfack5 <rincedonfack5@gmail.com>
2025-10-01 21:36:56 +0100

    modific

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Admin</title>
 
-   
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" />
@@ -86,7 +86,6 @@
 
 
 </head>
-@include('components.pwa-script')
 
 <body id="body" class="light bg-gray-50 text-gray-900">
 
