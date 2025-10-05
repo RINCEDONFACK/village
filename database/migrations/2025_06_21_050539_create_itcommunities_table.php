c2de182c0c15f20067f7f96ddfc95faa79b38c9e
rincedonfack5 <rincedonfack5@gmail.com>
2025-10-05 11:37:32 +0100

    modification

--- database/migrations/2025_06_21_050539_create_itcommunities_table.php
+++ database/migrations/2025_06_21_050539_create_itcommunities_table.php
@@ -10,7 +10,6 @@ return new class extends Migration {
         Schema::create('it_trainings', function (Blueprint $table) {
             $table->id();
             $table->string('image')->nullable();
-            $table->string('titre');
             $table->string('reference')->nullable();
             $table->text('description');
             $table->integer('duree')->nullable();
