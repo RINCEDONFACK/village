bf280ab005a532471e64bc10c33d17f1168d5ea2
rincedonfack5 <rincedonfack5@gmail.com>
2025-12-10 16:37:38 +0100

    modification photo

--- app/Http/Controllers/site/PhotoEvenementController.php
+++ app/Http/Controllers/site/PhotoEvenementController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Album;
 use Illuminate\Http\Request;
 
 class PhotoEvenementController extends Controller
 {
-    //
+    public function index()
+    {
+        // Correction: orderBy doit être appelé avant get()
+        $albums = Album::orderBy('created_at', 'desc')->get();
+
+        return view('front.photos.photo', compact('albums'));
+    }
+
+    public function show($id)
+    {
+        $album = Album::findOrFail($id);
+
+        return view('front.photos.photo_detail', compact('album'));
+    }
 }
